a7d2afd020fbd5dbd3dfe4aca97670117ea52701
Kvitkovsky Maxim <kvitkovsky1302@gmail.com>
2021-08-23 17:54:36 +0300

    add contacts to localestorage

--- src/App.js
+++ src/App.js
@@ -50,6 +50,18 @@ class App extends Component {
     );
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
